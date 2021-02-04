fadf0139c6faf89b337f4e0bf4b0ddd4e175f19c
Srinivas <srinivasb1999@gmail.com>
2021-02-04 23:54:15 +0530

    fix if condition for adding selected class

--- Site Header/index.js
+++ Site Header/index.js
@@ -2,5 +2,6 @@ document.addEventListener('click', (event) => {
     document.querySelectorAll('.header-item').forEach(element => {
         element.classList.remove('isSelected');
     });
-    event.target.classList.add('isSelected');
+    if(event.target.classList.contains('header-item'))
+        event.target.classList.add('isSelected');
 })
