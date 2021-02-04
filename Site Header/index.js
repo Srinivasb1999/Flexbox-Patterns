6663566db8127c763bc7dcb73feb4117ccf83c06
Srinivas <srinivasb1999@gmail.com>
2021-02-05 00:04:18 +0530

    add formatting

--- Site Header/index.js
+++ Site Header/index.js
@@ -2,6 +2,6 @@ document.addEventListener('click', (event) => {
     document.querySelectorAll('.header-item').forEach(element => {
         element.classList.remove('isSelected');
     });
-    if(event.target.classList.contains('header-item'))
+    if (event.target.classList.contains('header-item'))
         event.target.classList.add('isSelected');
 })
